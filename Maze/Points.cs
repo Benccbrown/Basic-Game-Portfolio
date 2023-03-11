e5a9dc55017d1db7b9ab9c45c93038e6dc378cd9
Ben Brown <61976248+Benccbrown@users.noreply.github.com>
2023-03-11 16:02:50 +0000

    A point system for found items

---
+++ Maze/Points.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{   
+    public static int eggs = 0;
+    Text egg;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        egg = GetComponent<Text> ();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    { //this is what the text will say on the screen saying how many easter eggs have been found
+        egg.text = ("Easter eggs: " + eggs);
+    }
+}
